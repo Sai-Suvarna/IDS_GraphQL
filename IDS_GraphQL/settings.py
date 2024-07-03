4de7c1badf37be7e0f88c0bead18f437da6894bb
Sai Suvarna Yandapalli <115972418+Sai-Suvarna@users.noreply.github.com>
2024-07-03 17:58:01 +0530

    Update settings.py

--- IDS_GraphQL/settings.py
+++ IDS_GraphQL/settings.py
@@ -177,7 +177,7 @@ GRAPHQL_JWT = {
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
     'JWT_VERIFY_EXPIRATION': True,
     'JWT_LONG_RUNNING_REFRESH_TOKEN': True,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(minutes=5),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=24),
     'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),    
     'JWT_SECRET_KEY': 'W2IjUe3kllnnZGWGDnRj8R3EukdojlSM8OlLUIJSvkA',
     'JWT_ALGORITHM': 'HS256',
