c3f26074b06a70c2d2926fe56babdfca8d87c281
Sai Suvarna Yandapalli <115972418+Sai-Suvarna@users.noreply.github.com>
2024-07-03 17:05:25 +0530

    Update urls.py

--- IDS_GraphQL/IDS_GraphQL/urls.py
+++ IDS_GraphQL/IDS_GraphQL/urls.py
@@ -7,12 +7,16 @@ from graphene_file_upload.django import FileUploadGraphQLView
 from Authentication.schema import login_schema
 from Core.schema import productdetails_schema
 
+from idscore.schema import idscore_schema
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', csrf_exempt(GraphQLView.as_view(graphiql=False, schema=login_schema))),
+    path('login/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=login_schema))),
     path('productdetails/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=productdetails_schema))),
+    path('idsdetails/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=idscore_schema))),
+
     path('searchword/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=wordsearch_schema))),
     path('upload/', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True, schema=imageupload_schema))),
     
