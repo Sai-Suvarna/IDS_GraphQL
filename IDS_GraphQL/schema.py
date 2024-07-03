993e053bf562ae2b812c9b57bf1d5f3e58806ed6
Sai Suvarna Yandapalli <115972418+Sai-Suvarna@users.noreply.github.com>
2024-07-03 17:57:20 +0530

    Update schema.py

--- IDS_GraphQL/schema.py
+++ IDS_GraphQL/schema.py
@@ -2,11 +2,12 @@ import graphene
 import graphql_jwt
 from Authentication.schema import login_schema
 from Core.schema import productdetails_schema
+from idscore.schema import idscore_schema
 
-class Query(login_schema.Query, productdetails_schema.Query, graphene.ObjectType):
+class Query(login_schema.Query, productdetails_schema.Query, idscore_schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(login_schema.Mutation, productdetails_schema.Mutation, graphene.ObjectType):
+class Mutation(login_schema.Mutation, productdetails_schema.Mutation, idscore_schema.Mutation , graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     refresh_token = graphql_jwt.Refresh.Field()
     verify_token = graphql_jwt.Verify.Field()    
