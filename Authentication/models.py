f2bc2662e60268aec3f538946b688b28c367b16f
Sai Suvarna Yandapalli <115972418+Sai-Suvarna@users.noreply.github.com>
2024-07-12 15:42:35 +0530

    Update models.py

--- Authentication/models.py
+++ Authentication/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class LoginManager(BaseUserManager):
     def create_user(self, username, phone_num, email, password=None, **extra_fields):
+        """
+        Creates and saves a regular user with the given username, phone number, email, and password.
+        """
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
@@ -15,13 +18,18 @@ class LoginManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-
+    """
+    Creates and saves a superuser with the given username, phone number, email, and password.
+    """
     def create_superuser(self, username, phone_num, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         return self.create_user(username, phone_num, email, password, **extra_fields)
 
+# Custom User model for Login
+
 class Login(AbstractBaseUser):
+    userId = models.AutoField(primary_key=True)  
     username = models.CharField(max_length=100, unique=True)
     phone_num = models.CharField(max_length=15, unique=True)
     designation = models.CharField(max_length=100)
@@ -32,10 +40,11 @@ class Login(AbstractBaseUser):
     status = models.CharField(max_length=100)
     password = models.CharField(max_length=128)
     
+    # Required fields for authentication
     REQUIRED_FIELDS = ['phone_num', 'email']
     USERNAME_FIELD = 'username'
 
     objects = LoginManager()
 
     def get_username(self):
-        return self.username
+        return self.username     # Returns the username of the user.
